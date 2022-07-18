e644f608d79f328a76f71f8542109cc7a844fc83
Arcade <kafkafait@gmail.com>
2022-07-18 14:10:49 -0400

    added a draft of the Employee Self-Service Menu
    this can be updated or deleted as necessary, it's merely a draft of what the self-service menu could look like once other functionality is added.

---
+++ src/ESSMenu.java
@@ -0,0 +1,17 @@
+
+public class ESSMenu {
+    /* drafting the employee self-service menu AE [7/18/2022]*/
+
+    public static void essmenu() {
+        System.out.println ("--------------------------------");
+        System.out.println ("WELCOME TO THE MCMILLAN EMPLOYEE SELF-SERVICE SYSTEM");
+        System.out.println ("Please select from the following options and enter the");
+        System.out.println ("associated keyword! I.E. (for [PAY] PAYROLL, you would enter \"PAY\").");
+        System.out.println ("[PAY] PAYROLL");
+        System.out.println ("[BEN] BENEFITS");
+        System.out.println ("[INF] PERSONAL INFO");
+        System.out.println ("[TAX] TAX INFORMATION");
+        System.out.println ("--------------------------------");
+    }
+
+}
