c66bc884f8c688cbdc75a6dc62d39700c2102994
astrickland8 <102337531+astrickland8@users.noreply.github.com>
2022-07-26 05:52:04 -0700

    New items

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,6 +2,7 @@ public class MainClass{
     /*main class contains the main method for execution
     AE[7/18/2022]*/
     public static void main(String[] arg) {
+        ESSMenu.essmenu();
 
     }
 
